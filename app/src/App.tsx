6bad589724f3fbf026439ad20ff2cf72b6632a1c
bruno henrique <bh.bsb2011@hotmail.com>
2023-04-30 15:47:37 -0300

    add menu and node counter

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,21 +1,24 @@
-import { useEffect, useState } from "react";
-import { ForceGraph3D } from "react-force-graph";
+import { useEffect, useRef, useState } from 'react';
+import { ForceGraph2D } from 'react-force-graph';
+import './App.css';
 
 const INITIAL_NODE_NUMBERS = 15;
 const initialNodes = Array.from({ length: INITIAL_NODE_NUMBERS }, (_, i) => i);
 
 export default function App() {
-  const [data, setData] = useState({
-    nodes: initialNodes.map((index) => ({
-      id: index
+  const fgRef = useRef();
+
+  const [data, setData] = useState<any>({
+    nodes: initialNodes.map(index => ({
+      id: index,
     })),
-    links: []
+    links: [],
   });
 
   useEffect(() => {
     setInterval(() => {
       // Add a new connected node every second
-      setData(({ nodes, links }) => {
+      setData(({ nodes, links }: any) => {
         const nodesLength = nodes.length - 1;
         const sourceRandomNode = Math.ceil(Math.random() * nodesLength);
         let targetRandomNode = Math.ceil(Math.random() * nodesLength);
@@ -28,15 +31,52 @@ export default function App() {
           nodes: newNodes,
           links: [
             ...links,
-            { source: sourceRandomNode, target: targetRandomNode }
-          ]
+            { source: sourceRandomNode, target: targetRandomNode },
+          ],
         };
       });
-    }, 5000);
+    }, 3000);
   }, []);
+
+  function handleNodeChange(number: number) {
+    const newNodes = Array.from({ length: number }, (_, i) => i);
+    setData({
+      nodes: newNodes.map(id => ({ id })),
+      links: [],
+    });
+  }
+
   return (
     <div className="App">
-      <ForceGraph3D graphData={data} nodeLabel="id" nodeAutoColorBy="group" />,
+      <ForceGraph2D
+        ref={fgRef}
+        cooldownTicks={100}
+        onEngineStop={() => fgRef.current.zoomToFit(400)}
+        graphData={data}
+        nodeLabel="id"
+        nodeAutoColorBy="group"
+        linkDirectionalArrowLength={2.5}
+        linkDirectionalArrowRelPos={1}
+        linkCurvature={0.25}
+      />
+      <div className="form">
+        <label>Quantide de nós:</label>
+        <input
+          type="number"
+          placeholder="Digite a quatidade de nós"
+          className="nodeInputs"
+          value={data.nodes.length}
+          onChange={e => handleNodeChange(parseInt(e.target.value))}
+        />
+        <button>Começar</button>
+
+        <label>Lista de transações</label>
+        <div className="list">
+          <span>Fulano {'>'} Beltrano R$ 12,00</span>
+          <span>Fulano {'>'} Beltrano</span>
+          <span>Fulano {'>'} Beltrano</span>
+        </div>
+      </div>
     </div>
   );
 }
